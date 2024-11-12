acd04c82c6138c73ca35b484b11029cf7224491f
Rimsha Aslam <rimsha.aslam.230@gmail.com>
2024-11-13 00:47:46 +0500

    aos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 import { useEffect } from "react";
 import Aos from "aos";
 
